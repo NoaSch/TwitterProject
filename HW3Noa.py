32c46840db78335523889504f03794342a2959cc
NoaSch <noasch4@gmail.com>
2018-01-03 20:25:21 +0200

    from noa to danit:)

--- HW3Noa.py
+++ HW3Noa.py
@@ -24,28 +24,12 @@ from sklearn.model_selection import GridSearchCV
 import re
 from collections import Counter
 import itertools
+from sets import Set
 
 twitterDataSet = pd.read_csv("c:\corpus\gender-classifier-DFE-791531.csv", encoding='latin1')
 #twitterDataSet = pd.read_csv("c:\gender-classifier-DFE-791531.csv")
-print(twitterDataSet.head())
+#print(twitterDataSet.head())
 
-
-
-'''
-def normalize_text(text):
-    # just in case
-    text = text.lower()
-
-    # remove punctuation that is not word-internal (e.g., hyphens, apostrophes)
-    text = re.sub('\s\W', ' ', text)
-    text = re.sub('\W\s', ' ', text)
-
-
-    # make sure we didn't introduce any double spaces
-    text = re.sub('\s+', ' ', text)
-
-    return text
-'''
 
 #clean a tweet
 
@@ -54,58 +38,114 @@ def cleanData(text):
     #print("******************************")
     text = text.lower()
 
-    # remove punctuation that is not word-internal (e.g., hyphens, apostrophes)
+    # remove punctuation that is not word-internal
     text = re.sub('\s\W', ' ', text)
     text = re.sub('\W\s', ' ', text)
 
-    # make sure we didn't introduce any double spaces
+    # remove  any double spaces
     text = re.sub('\s+', ' ', text)
 
     stop_words = stopwords.words('english') + list(string.punctuation)
     stemmer = SnowballStemmer("english")
 
-    #for doc in data:
-    docAfterTokenize = word_tokenize(text) #text after tokenize (from full sentences to separated words)
-    filteredTwit = [word for word in docAfterTokenize if word not in stop_words]
+    twitAfterTokenize = word_tokenize(text) #text after tokenize (from full sentences to separated words)
+    filteredTwit = [word for word in twitAfterTokenize if word not in stop_words]
     twitAfterStemming = [stemmer.stem(word) for word in filteredTwit] #stem each word in the text
     twitAfterStemmingStr = ' '.join(twitAfterStemming)
     return twitAfterStemmingStr
 
-
+#update the corpus with the clean twits
 twitterDataSet['text_norm'] = [cleanData(s) for s in twitterDataSet['text']]
 #twitterDataSet['description_norm'] = [cleanData(s) for s in twitterDataSet['description']]
 
 
 
-termsPerCat = {} #dictionary of all categories and for each one- list of all terms (after cleaning) in category
+#termsPerCat = {} #dictionary of all categories and for each one- list of all terms (after cleaning) in category
 
 #get all the common terms and their frequency in a category
 def getCommonTermsPerCat (gender):
-    allTermsInCat = list(itertools.chain(*termsPerCat[gender]))#make list of all terms (transfer list of lists to one list)
+    allTermsInCat = list(itertools.chain(*twitsPerGender[gender]))#make list of all terms (transfer list of lists to one list)
     termsCounter = Counter(allTermsInCat)
-    return (termsCounter.most_common(10))
+    return (termsCounter.most_common(30))
 
 
 #get all the common terms in the corpus
-def getCommonMedTerms (cleanTextsPerTerms):
-    global commonTermsInCorpus
-    allTerms = list(itertools.chain(*cleanTextsPerTerms))#make list of all terms (transfer list of lists to one list)
-    commonTermsInCorpus = [term for term, word_count in Counter(allTerms).most_common(20)]
+#def getCommonMedTerms (cleanTextsPerTerms):
+#    global commonTermsInCorpus
+#    allTerms = list(itertools.chain(*cleanTextsPerTerms))#make list of all terms (transfer list of lists to one list)
+#    commonTermsInCorpus = [term for term, word_count in Counter(allTerms).most_common(20)]
 
 
-    #get the tweet foreach gender
+#get the tweet foreach gender
 twitsPerGender ={};
 twitsPerGender['male'] = [];
 twitsPerGender['female'] = [];
 twitsPerGender['unknown'] = [];
 twitsPerGender['brand'] = [];
-for index, row in twitterDataSet.iterrows():
-    twitsPerGender[row['gender']].append(row['text_norm'])
+for index, row in twitterDataSet.iterrows(): # iterate each row
+    #twitsPerGender[row['gender']].append(row['text_norm']) #if we want that twit will save as string not list
+    twitsPerGender[row['gender']].append(row['text_norm'].split())
 #count the twits in each category
 print("Class Distribution:")
 print("***************************************************")
 for key, value in twitsPerGender.iteritems():
-    print('{}: {}'.format(key,len(value)))
-    #print (len(value))
+    print('{}: {} twits'.format(key,len(value)))
+commonWords = [];
+##get the terms distribution
+table_of_most_frequent = {};# array of tuples - term and requenchy
+most_freq_byCat = {};
+temp = {};
+commTermsInAll = []
+commSet = Set(commTermsInAll);
+for gen in twitsPerGender:
+    #print (gen)
+    lst = getCommonTermsPerCat(gen);
+    table_of_most_frequent[gen] = lst;
+    most_freq_byCat[gen] = [];
+    temp[gen] = [];
 
+for gen in twitsPerGender:
+  for commTerm, word_count in table_of_most_frequent[gen]:
+      most_freq_byCat[gen].append(commTerm)
 
+for gen in twitsPerGender:
+    ##remove terms that apear in all categories as common
+    for commTerm in most_freq_byCat[gen]:
+        if(commTerm not in commSet):
+            counter = 1
+            for otherGen in twitsPerGender:
+                if(otherGen != gen and commTerm in most_freq_byCat[otherGen]):
+                    counter = counter +1
+            if(counter == len(twitsPerGender)):
+                commSet.add(commTerm)
+                #print(commTerm)
+
+#remove the terms that common in all categories
+for gen in twitsPerGender:
+    for i in table_of_most_frequent[gen]:
+        if i[0] not in commSet:
+            temp[gen].append(i)
+    table_of_most_frequent[gen] = temp[gen];
+
+#plot the results
+for gen in twitsPerGender:
+    df3 = pd.DataFrame(table_of_most_frequent[gen], columns = ['Word', 'Count'])
+    ax = df3.plot.bar(title ="most frequent terms in category: " + gen, x='Word',y='Count')
+    x_offset = -0.4
+    y_offset = 2
+    for p in ax.patches:
+        b = p.get_bbox()
+        val = b.y1 + b.y0
+        ax.annotate(val, ((b.x0 + b.x1)/2 + x_offset, b.y1 + y_offset))
+    #print(gen)
+    #print("**************************")
+    #print(df3)
+df2 = pd.DataFrame(table_of_most_frequent)
+df2.transpose()
+print("")
+print("terms frequency") #without terms that common apear in alll category
+print (df2)
+
+print("")
+print("the terms that are common in all categories")
+print (commSet)
