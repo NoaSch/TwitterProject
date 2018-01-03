18c6504fed419c54fa36cb8cab13d1a4e6becbfa
NoaSch <noasch4@gmail.com>
2018-01-03 14:44:00 +0200

    second

--- HW3Noa.py
+++ HW3Noa.py
@@ -2,6 +2,6 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 # have to use latin1 even though it results in a lot of dead characters
-twigen = pd.read_csv("c:\gender-classifier-DFE-791531.csv", encoding='latin1')
-twigen.head()
+twitterDataSet = pd.read_csv("c:\gender-classifier-DFE-791531.csv", encoding='latin1')
+twitterDataSet.head()
 
